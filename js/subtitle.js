a1841597596ae4901aa255c567aca31a126961b1
Sudipta Chatterjee <sudipta.referral@gmail.com>
2013-08-20 01:27:10 -0700

    Intermediate commit
    Introduced three step process where you first load the video, add the
    transcript, and then start adding dialogues.
    Still to do: 1. YouTube API to remove "started playing" button,
    2. fix the remove top bug

--- js/subtitle.js
+++ js/subtitle.js
@@ -1,5 +1,6 @@
 var timerStarted = false;
 var startTime = '';
+var allSubtitlesArray = [];
 
 // Returns an HH:MM:SS.SSS formatted timestamp that shows how far it has
 // been since the first click
@@ -21,34 +22,12 @@ function getTimeFromStart() {
 
 $(document).ready(function () {
 
-    function addToSubtitle (start, end, text) {
-        var presentText = $("#sbv").html();
-        newText = presentText + start + ',' + end + '\n' + text +'\n';
-        $("#sbv").html(newText);
-    }
+    // Hide everything except the video div
+    $("#transcriptDiv").hide();
+    $("#clickNextDiv").hide();
+    $("#videoDiv").show();
 
-    function removeTop() {
-        // Removes the topmost line from the subtitle text and returns it
-        var currentText = $("#subtitle").val();
-        if (!currentText) {
-            $("#add").addClass("disabled");
-            $("#gap").addClass("disabled");
-
-            return '';
-        }
-
-        currentText = currentText.split('\n');
-
-        
-        var topMostText = '';
-        while (topMostText.length == 0) {
-            topMostText = currentText.splice(0,1)[0];
-        }
-        $("#subtitle").val(currentText.join('\n'));
-
-        return topMostText;
-    }
-
+    // Make them add the video first
     $("#addVideo").on('click',function() {
         var youSrc = $("#youtubeSrc").val();
         if (youSrc.length == 0) {
@@ -65,21 +44,67 @@ $(document).ready(function () {
             .attr('width',"420")
             .attr('height',"275");
 
-        $("#addVideo").addClass("disabled").removeClass("btn-info");
-        $("#youtubeSrc").addClass("disabled");
-        $("#add").addClass("btn-info").removeClass("disabled");
-        $("#gap").addClass("btn-inverse").removeClass("disabled");
+        $("#addVideo").hide();
+        $("#youtubeSrc").hide();
+        $("#transcriptDiv").show();
     });
 
-    var lastTimeDiff = '00:00:00';
-    $("#add").on('click', function() {
-        if (timerStarted == false) {
-            timerStarted = true;
-            startTime = moment();
-            $("#sbv").html("");
+
+    var transcript = [];
+    $("#addTranscript").on('click', function() {
+        var rawText = $("#transcriptText").val();
+        if  (rawText.length == 0) {
+            alert("Blaargh! Add the transcript first");
             return false;
         }
 
+        // Parse and load the text
+        var allLines = rawText.split('\n');
+        var whiteSpacePattern = new RegExp("\S");
+        $.each(allLines, function(idx, line) {
+            if (whiteSpacePattern.test(line)) {
+                transcript.push(line);
+            }
+        });
+
+        $("#transcriptDiv").hide();
+        $("#clickNextDiv").show();
+    });
+
+    function addToSubtitle (start, end, text) {
+        var presentText = $("#sbv").html();
+        newText = presentText + start + ',' + end + '\n' + text +'\n';
+        $("#sbv").html(newText);
+    }
+
+    function removeTop() {
+        // Removes the topmost line from the transcript text and returns it
+        if (transcript.length == 0) {
+            console.log("Nothing in transcript");
+            $("#textStart").addClass("disabled");
+            $("#gap").addClass("disabled");
+            return '';
+        }
+        console.log("About to remove topmost text");
+
+        var topMostText = transcript.splice(0,1);
+        $("#nextDialogue").html(topMostText);
+        console.log("Next to remove "+transcript[0]);
+
+        return topMostText;
+    }
+
+    var lastTimeDiff = '00:00:00';
+    $("#startTimer").on('click', function() {
+        timerStarted = true;
+        startTime = moment();
+        $("#sbv").html();
+        return false;
+        $("#textStart").removeClass("disabled");
+        $("#gap").removeClass("disabled");
+    });
+
+    $("#textStart").on('click', function() {
         timeDiffNow = getTimeFromStart();
         var currentText = removeTop();
         addToSubtitle(lastTimeDiff,timeDiffNow,currentText);
